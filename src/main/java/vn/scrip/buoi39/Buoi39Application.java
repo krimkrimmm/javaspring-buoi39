4bd402f9841385ec9152dd0b1678c4386a6a0075
krimkrimmm <147626046+krimkrimmm@users.noreply.github.com>
2025-05-12 10:51:14 +0700

    javspring-buoi39

---
+++ src/main/java/vn/scrip/buoi39/Buoi39Application.java
@@ -0,0 +1,19 @@
+package vn.scrip.buoi39;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class Buoi39Application {
+	public static void main(String[] args)
+	{
+		SpringApplication.run(Buoi39Application.class, args);
+	}
+
+}
+
+
+
+
+
+
+
+
---
+++ src/test/java/vn/scrip/buoi38_bvn/Buoi38BvnApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.scrip.buoi38_bvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Buoi38BvnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
